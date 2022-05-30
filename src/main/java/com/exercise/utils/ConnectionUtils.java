20edcd96466635edb5f8fc66adf4dfc8be3b05f9
xNamNgo <hoangnam11pdp@gmail.com>
2022-05-30 19:50:53 +0700

    Sửa tên package

--- src/main/java/com/exercise/utils/ConnectionUtils.java
+++ src/main/java/com/exercise/utils/ConnectionUtils.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ControllerUltils {
+public class ConnectionUtils {
  	private static final String DB_URL = "jdbc:mysql://localhost:3306/javasql42022";
  	private static final String USER = "root";
  	private static final String PASS = "0915404712Nam";
