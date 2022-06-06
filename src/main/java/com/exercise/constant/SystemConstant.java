25418f3c905a527a6b30c2e118666cc1bd8c5b3c
xNamNgo <hoangnam11pdp@gmail.com>
2022-06-06 12:56:11 +0700

    Update code,values types for building

--- src/main/java/com/exercise/constant/SystemConstant.java
+++ src/main/java/com/exercise/constant/SystemConstant.java
@@ -2,5 +2,12 @@ package com.exercise.constant;
 
 public class SystemConstant {
 	public static final String ONE_EQUAL_ONE = "1 = 1";
+	
+	public static final String TANG_TRET_CODE = "tang-tret";
+	public static final String NGUYEN_CAN_CODE = "nguyen-can";
+	public static final String NOI_THAT_CODE = "noi-that";
+	public static final String TANG_TRET = "Tầng trệt";
+	public static final String NGUYEN_CAN = "Nguyên căn";
+	public static final String NOI_THAT = "Nội Thất";
 
 }
