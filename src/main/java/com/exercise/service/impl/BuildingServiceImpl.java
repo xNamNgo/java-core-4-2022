d75f4dc63e33f62ce4efa995faed91e2bfb37abe
xNamNgo <hoangnam11pdp@gmail.com>
2022-06-06 12:57:45 +0700

    Update code

--- src/main/java/com/exercise/service/impl/BuildingServiceImpl.java
+++ src/main/java/com/exercise/service/impl/BuildingServiceImpl.java
@@ -2,13 +2,13 @@ package com.exercise.service.impl;
 
 import java.util.ArrayList;
 
+import com.exercise.constant.SystemConstant;
 import com.exercise.dao.BuildingDao;
 import com.exercise.dao.entity.BuildingEntity;
 import com.exercise.dao.impl.BuildingDaoImpl;
 import com.exercise.model.dto.BuildingDTO;
 import com.exercise.model.output.BuildingOutput;
 import com.exercise.service.BuildingService;
-import com.exercise.utils.TypeBuildingUtils;
 
 public class BuildingServiceImpl implements BuildingService {
 	private BuildingDao buildingDao = new BuildingDaoImpl();
@@ -25,12 +25,31 @@ public class BuildingServiceImpl implements BuildingService {
 			buildingOutput.setName(item.getName());
 			buildingOutput.setAddress(item.getStreet() + " - " + item.getWard() + " - " + item.getDistrict());
 			buildingOutput.setFloorarea(item.getFloorarea());
-			StringBuilder type = TypeBuildingUtils.formatType(item.getType());
-			buildingOutput.setType(type.toString());
+			StringBuilder typeBuilding = getTypesBuilding(item.getType());
+			buildingOutput.setType(typeBuilding.toString());
 			results.add(buildingOutput);
 		}
 		return results;
 	}
 
+	public static StringBuilder getTypesBuilding(String typeBuilding) {
+		String[] typeBuildingSplit = typeBuilding.split(",");
+		StringBuilder results = new StringBuilder();
+		for (int i = 0; i < typeBuildingSplit.length; i++) {
+			if (typeBuildingSplit[i].equals(SystemConstant.TANG_TRET_CODE)) {
+				results.append(SystemConstant.TANG_TRET);
+			}
+			if (typeBuildingSplit[i].equals(SystemConstant.NGUYEN_CAN_CODE)) {
+				results.append(SystemConstant.NGUYEN_CAN);
+			}
+			if (typeBuildingSplit[i].equals(SystemConstant.NOI_THAT_CODE)) {
+				results.append(SystemConstant.NOI_THAT);
+			}
+			if (i < (typeBuildingSplit.length - 1)) {
+				results.append(" , ");
+			}
+		}
+		return results;
+	}
 
 }
